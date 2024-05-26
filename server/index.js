0cb6bf8d3874a477e526106e995a47e19d819200
iLaRae <115182525+iLaRae@users.noreply.github.com>
2024-05-26 14:07:56 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("server is Running");
+});
+
+
+app.listen(5000, console.log("Server started on Port 5000"))
